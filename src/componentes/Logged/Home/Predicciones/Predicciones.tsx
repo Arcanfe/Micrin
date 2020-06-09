11ed52fb4a0cd652c19ef17c660aeb28faa9b465
Andres Ariza <afelipe2599@gmail.com>
2020-06-08 23:04:56 -0500

    Correcciones analitica-inventario. Version Final

--- src/componentes/Logged/Home/Predicciones/Predicciones.tsx
+++ src/componentes/Logged/Home/Predicciones/Predicciones.tsx
@@ -40,18 +40,26 @@ const Predicciones: React.FC<modalBodyFormProps> = (props: modalBodyFormProps) =
     }
 
     const mostrarDetalles = async (e:any, f:any) => {
+        console.log(f);
+        console.log(e);
         try {
-            console.log(f);
+            setPlato(f);
             const result = await axios.get('https://inventario-services.herokuapp.com/invservice/plato/getPlato/?nombre=' + f, config);
             let data = result.data.receta
             for(let i = 0 ; i< data.length ; i++ ) {
                 data[i].nombreIng = await getNombre(data[i].codigo_spro);
                 data[i].cantIng = await getCantidad(data[i].codigo_spro);
-                if(data[i].cantIng >= (data[i].cantidad * e)){
+                console.log(data[i].cantidadxplato);
+                console.log(e);
+                console.log(data[i].cantIng);
+                if(data[i].cantIng >= (data[i].cantidadxplato * e)){
+                    
                     data[i].posible = true;
+                    data[i].neces = 0;
                 }
                 else{
                     data[i].posible = false;
+                    data[i].neces = ((data[i].cantidadxplato * e) - data[i].cantIng);
                 }
             }
             setStockRecetas(data);
@@ -79,7 +87,10 @@ const Predicciones: React.FC<modalBodyFormProps> = (props: modalBodyFormProps) =
                 <p>Recuerda que a medida que se van realizando los registros de venta, nuestro algoritmo va afinando y creando predicciones para ti.</p>
                 <p>Selecciona un plato en la lista para ver si tienes los ingredientes necesarios.</p>
                     {predicciones.map(pr => (
-                        <Button fluid onClick={() => {mostrarDetalles(pr.cantidad, pr.nombre_plato)}} > + {pr.cantidad_plato} -> {pr.plato} - {pr.precision}</Button>
+                        <div>
+                            <p>+ {pr.cantidad_plato} -> {pr.plato} - {pr.precision}</p>
+                            <Button onClick={() => {mostrarDetalles(pr.cantidad_plato, pr.plato)}} >Ver disponibilidad</Button>
+                        </div>
                     ))}
                 <p>*Recuerda el ultimo valor presentado, es una predicción basada en el resultado de ventas realizadas con anterioridad. La probabilidad de certeza se indica junto al nombre.</p>
             </Segment>
@@ -87,12 +98,17 @@ const Predicciones: React.FC<modalBodyFormProps> = (props: modalBodyFormProps) =
             <Segment textAlign='left'>
                 <h4>Detalles para preparar {plato}</h4>
                 {stockRecetas.map(st => (
+                    <div>
+                   
                         <Label color={st.posible === true ? 'green' : 'red'}>
-                            Nombre del ingrediente {st.nombreIng} - Cantidad actual {st.cantIng} - {st.posible === true ? 'Es posible suplir la demanda.':'No es posible suplir la demanda.'}
+                            Nombre del ingrediente {st.nombreIng} - Cantidad actual {st.cantIng} - {st.posible === true ? 'Es posible suplir la demanda.':'No es posible suplir la demanda. Necesita comprar ' + st.neces + ' productos más.'}
                         </Label>
+                    </div>
                 ))}
                 <p>*Recuerda que aquellos productos de color rojo, indica que no existen suficientes ingredientes para realizar todos los platos previstos.</p>
             </Segment>
+            <br />
+            <p> </p>
         </Container>
     );
 }
